87d0cbce7448ec87b75c4abb3f028d2807b07b0a
mkalika <michael.kalika@gmail.com>
2014-11-18 18:42:38 +0200

    assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "vector", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  p_inversedMatrix <- NULL ## this variable stores the value of inversed matrix
+  
+  ## set the value of the matrix
+  set <- function(matr){
+    x <<- matr
+    p_inversedMatrix <<- NULL
+  }
+  
+  ## get the value of the matrix
+  get <- function() x
+  
+  ## set the value of the inverse matrix
+  setInverseMatrix <- function(inverseMatrix) {
+    p_inversedMatrix <<- inverseMatrix
+  }
+  
+  ## get the value of the inverse matrix
+  getInversedMatrix <- function() p_inversedMatrix
+  
+  list(set = set, get = get, setInverseMatrix = setInverseMatrix, 
+       getInversedMatrix = getInversedMatrix)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'.
+## cacheSolve function calculates the inverse matrix of the special "vector" created using makeCacheMatrix function. 
+## However, it first checks to see if the inverse matrix has already been calculated. If so, it gets the inverse matrix from the cache 
+## and skips the computation. Otherwise, it calculates the inverse matri and sets it in the cache via the setInverseMatrix function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {  
+  p_inversedMatrix <- x$getInversedMatrix()  # get the value of inversed matrix
+  
+  ## don't make inverse matrix calculation and return its value from cache if p_inversedMatrix is not NULL - meaning cached value exist
+  if (!is.null(p_inversedMatrix)) {
+    message("getting cached inversed matrix")
+    return(p_inversedMatrix)
+  }
+  
+  ## otherwise, calculate inverse matrix value and set it in cache
+  message("calculating inversed matrix")
+  data <- x$get() ## get matrix data for calculation
+  p_inversedMatrix <- solve(data, ...) ## calculate inverse matrix
+  x$setInverseMatrix(p_inversedMatrix) ## set calculated inverse matrix in cache
+  
+  p_inversedMatrix
 }
